e88fc1aa3d0b581610b2e075ff0f120c61c27575
Dmytro Kondratiuk <k-d-m@ukr.net>
2025-06-13 17:57:41 +0300

    format

--- winer.rb
+++ winer.rb
@@ -1,20 +1,20 @@
-class Winer
-	attr_reader :result, :bet
+class Winner
+  attr_reader :result, :bet
 
-	def initialize(result:, bet:)
-		@result = result
-		@bet = bet
-	end
+  def initialize(result:, bet:)
+    @result = result
+    @bet = bet
+  end
 
-	def check
+  def check
     return 'Invalid result or bet inputs' unless correct_inputs?
     return 1 if right_result?
-    return 0 if right_outcome?
+    return 0 if right_final_result?
 
     -1
   end
 
-	private
+  private
 
   def result_array
     @result_array ||= result.split(':')
@@ -24,7 +24,7 @@ class Winer
     @bet_array ||= bet.split(':')
   end
 
-	def correct_inputs?
+  def correct_inputs?
     result_array.size == 2 && bet_array.size == 2 && correct_inputs_values?
   end
 
@@ -33,18 +33,18 @@ class Winer
   end
 
   def correct_value?(value)
-	  /^\d+$/.match?(value) && value.to_i >= 0
+    /^\d+$/.match?(value) && value.to_i >= 0
   end
 
   def right_result?
     result_array[0].to_i == bet_array[0].to_i && result_array[1].to_i == bet_array[1].to_i
   end
 
-  def right_outcome?
-    (result_outcome >= 0) && (bet_outcome >= 0)
+  def right_final_result?
+    (final_result >= 0) && (bet_outcome >= 0)
   end
 
-  def result_outcome
+  def final_result
     result_array[0].to_i - result_array[1].to_i
   end
 
